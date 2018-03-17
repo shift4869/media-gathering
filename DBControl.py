abade8d7db3ccacfff5dc0efe8b00da7d61ccd9d
shift4869 <fantasy_world4423-git@yahoo.co.jp>
2018-03-17 10:45:38 +0900

    Remove comment

--- DBControl.py
+++ DBControl.py
@@ -20,23 +20,11 @@ pn = '?,?,?,?,?,?,?'
 del_sql = 'replace into DeleteTarget (' + p1 + ') values (' + pn + ')'
 
 
-# def TweetURLGet(id_str):
-#     url = "https://api.twitter.com/1.1/statuses/show.json"
-#     params = {
-#         "id": id_str,
-#     }
-
-#     tweet = Crawler.TwitterAPIRequest(url, params)
-#     return tweet["entities"]["media"][0]["expanded_url"]
-
-
 # id	img_filename	url	url_large
 # tweet_id	tweet_url	created_at	user_id	user_name	screan_name	tweet_text
 # saved_localpath	saved_created_at
 def DBFavUpsert(url, tweet, save_file_fullpath):
     url_orig = url + ":orig"
-    # save_file_path = os.path.join(save_path, os.path.basename(url))
-    # save_file_fullpath = os.path.abspath(save_file_path)
     td_format = '%a %b %d %H:%M:%S +0000 %Y'
     dts_format = '%Y-%m-%d %H:%M:%S'
 
