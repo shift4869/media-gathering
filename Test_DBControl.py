d01e88698b147626f49de50b6cc95bfb73a17109
shift <fantasy_world4423-git@yahoo.co.jp>
2018-11-25 11:02:40 +0900

    テスト実装

--- Test_DBControl.py
+++ Test_DBControl.py
@@ -18,13 +18,14 @@ class TestDBControl(unittest.TestCase):
         self.tweet_url_s = 'http://www.tweet.sample.com'
         self.save_file_fullpath_s = os.getcwd()
         self.tweet_s = self.__GetTweetSample(self.img_url_s)
+        self.del_tweet_s = self.__GetDelTweetSample()
 
     def __GetTweetSample(self, img_url_s):
         # ツイートオブジェクトのサンプルを生成する
         tweet_s = {}
         tweet_s.setdefault("entities", {})
         tweet_s["entities"].setdefault("media", [])
-        tweet_s["entities"]["media"].append({"expanded_url": img_url_s})
+        tweet_s["entities"]["media"].append({"expanded_url": self.tweet_url_s})
         tweet_s.setdefault("created_at", "Sat Nov 18 17:12:58 +0000 2018")
         tweet_s.setdefault("user", {})
         tweet_s.setdefault("id_str", "12345_id_str_sample")
@@ -32,6 +33,14 @@ class TestDBControl(unittest.TestCase):
         tweet_s["user"].setdefault("name", "shift_name_sample")
         tweet_s["user"].setdefault("screen_name", "_shift4869_screen_name_sample")
         tweet_s.setdefault("text", "tweet_text_sample")
+        return tweet_s
+
+    def __GetDelTweetSample(self):
+        # ツイートオブジェクトのサンプルを生成する
+        tweet_s = {}
+        tweet_s.setdefault("text", "@s_shift4869 PictureGathering run.\n2018/03/09 11:59:38 Process Done !!\nadd 1 new images. delete 0 old images.")
+        tweet_s.setdefault("created_at", "Sat Nov 18 17:12:58 +0000 2018")
+        tweet_s.setdefault("id_str", "12345_id_str_sample")
         return tweet_s
 
     def test_SQLText(self):
@@ -59,6 +68,11 @@ class TestDBControl(unittest.TestCase):
         pn = '?,?,?,?,?,?,?'
         expect = 'replace into DeleteTarget (' + p1 + ') values (' + pn + ')'
         actual = controlar._DBControlar__del_sql
+        self.assertEqual(expect, actual)
+
+        limit_s = 300
+        expect = 'select * from Favorite order by id desc limit {}'.format(limit_s)
+        actual = controlar._DBControlar__GetFavoriteSelectSQL(limit_s)
         self.assertEqual(expect, actual)
 
         limit_s = 300
@@ -106,6 +120,116 @@ class TestDBControl(unittest.TestCase):
             fav_sql_s = controlar._DBControlar__fav_sql
             mocksql.connect().cursor().execute.assert_called_once_with(fav_sql_s, param_s)
 
+    def test_DBFavSelect(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            controlar = DBControl.DBControlar()
+            expect = ("rowid_sample",) + controlar._DBControlar__GetUpdateParam(self.img_url_s, self.tweet_s, self.save_file_fullpath_s)
+            mocksql.connect().cursor().execute.return_value = [expect]
+
+            # DB操作を伴う操作を行う
+            limit_s = 300
+            actual = controlar.DBFavSelect(limit_s)
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            fav_select_sql_s = controlar._DBControlar__GetFavoriteSelectSQL(limit_s)
+            mocksql.connect().cursor().execute.assert_called_once_with(fav_select_sql_s)
+
+            # 取得した値の確認
+            self.assertEqual(self.img_url_s + ":orig", actual[0][2])
+            self.assertEqual(self.tweet_url_s, actual[0][5])
+            self.assertEqual(expect, actual[0])
+
+    def test_DBRetweetUpsert(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            mocksql.connect().commit.return_value = 'commit done'
+            controlar = DBControl.DBControlar()
+
+            # DB操作を伴う操作を行う
+            controlar.DBRetweetUpsert(self.img_url_s, self.tweet_s, self.save_file_fullpath_s)
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            param_s = controlar._DBControlar__GetUpdateParam(self.img_url_s, self.tweet_s, self.save_file_fullpath_s)
+            retweet_sql_s = controlar._DBControlar__retweet_sql
+            mocksql.connect().cursor().execute.assert_called_once_with(retweet_sql_s, param_s)
+
+    def test_DBRetweetSelect(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            controlar = DBControl.DBControlar()
+            expect = ("rowid_sample", "is_exist_save_file_flag_sample") + controlar._DBControlar__GetUpdateParam(self.img_url_s, self.tweet_s, self.save_file_fullpath_s)
+            mocksql.connect().cursor().execute.return_value = [expect]
+
+            # DB操作を伴う操作を行う
+            limit_s = 300
+            actual = controlar.DBRetweetSelect(limit_s)
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            retweet_select_sql_s = controlar._DBControlar__GetRetweetSelectSQL(limit_s)
+            mocksql.connect().cursor().execute.assert_called_once_with(retweet_select_sql_s)
+
+            # 取得した値の確認
+            self.assertEqual(self.img_url_s + ":orig", actual[0][3])
+            self.assertEqual(self.tweet_url_s, actual[0][6])
+            self.assertEqual(expect, actual[0])
+
+    def test_DBRetweetFlagUpdate(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            mocksql.connect().commit.return_value = 'commit done'
+            controlar = DBControl.DBControlar()
+
+            # DB操作を伴う操作を行う
+            controlar.DBRetweetFlagUpdate(self.img_filename_s)
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            retweet_flag_update_sql_s = controlar._DBControlar__GetRetweetFlagUpdateSQL(self.img_filename_s)
+            mocksql.connect().cursor().execute.assert_called_once_with(retweet_flag_update_sql_s)
+
+    def test_DBDelInsert(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            mocksql.connect().commit.return_value = 'commit done'
+            controlar = DBControl.DBControlar()
+
+            # DB操作を伴う操作を行う
+            controlar.DBDelInsert(self.del_tweet_s)
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            param_s = controlar._DBControlar__GetDelUpdateParam(self.del_tweet_s)
+            mocksql.connect().cursor().execute.assert_called_once_with(controlar._DBControlar__del_sql, param_s)
+
+    def test_DBDelSelect(self):
+        # DB操作をモックに置き換える
+        with patch('DBControl.sqlite3') as mocksql, freezegun.freeze_time('2018-11-18 17:12:58'):
+            mocksql.connect().cursor().execute.return_value = 'execute sql done'
+            mocksql.connect().commit.return_value = 'commit done'
+            controlar = DBControl.DBControlar()
+
+            expect = ("rowid_sample",) + controlar._DBControlar__GetDelUpdateParam(self.del_tweet_s)
+            mocksql.connect().cursor().execute.return_value = [expect]
+
+            t = date.today() - timedelta(1)
+            w = "delete_done = 0 and created_at < '{}'".format(t.strftime('%Y-%m-%d'))
+            expect_select_sql_s = "select * from DeleteTarget where " + w
+            u = "delete_done = 1, deleted_at = '{}'".format(t.strftime('%Y-%m-%d'))
+            expect_update_sql_s = "update DeleteTarget set {} where {}".format(u, w)
+
+            # DB操作を伴う操作を行う
+            actual = controlar.DBDelSelect()
+
+            # DB操作が規定の引数で呼び出されたことを確認する
+            mocksql.connect().cursor().execute.assert_any_call(expect_select_sql_s)
+            mocksql.connect().cursor().execute.assert_any_call(expect_update_sql_s)
+
+            # 取得した値の確認
+            self.assertEqual(expect, actual[0])
 
 if __name__ == "__main__":
     unittest.main()
