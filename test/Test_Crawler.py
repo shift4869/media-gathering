898b4dd3858cdba86f97a5a8eed3c5208e83d463
s_shift <fantasy_world4423-git@yahoo.co.jp>
2020-02-13 14:11:55 +0900

    WIP

--- test/Test_Crawler.py
+++ test/Test_Crawler.py
@@ -21,14 +21,14 @@ import sys
 import time
 import unittest
 from contextlib import ExitStack
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 from logging import WARNING, getLogger
 
 import requests
 from mock import MagicMock, PropertyMock, patch
 from requests_oauthlib import OAuth1Session
 
-from PictureGathering import Crawler, DBController, WriteHTML
+from PictureGathering import Crawler
 
 logger = getLogger("root")
 logger.setLevel(WARNING)
@@ -492,14 +492,16 @@ class TestCrawler(unittest.TestCase):
                 "screen_name": crawler.user_name,
                 "page": i,
                 "count": crawler.count,
-                "include_entities": 1  # ツイートのメタデータ取得。複数枚の画像取得用。
+                "include_entities": 1,  # ツイートのメタデータ取得。これしないと複数枚の画像に対応できない。
+                "tweet_mode": "extended"
             }
             self.assertIsNotNone(crawler.TwitterAPIRequest(url, params))
 
         url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         params = {
             "count": crawler.count,
-            "include_entities": 1
+            "include_entities": 1,
+            "tweet_mode": "extended"
         }
         self.assertIsNotNone(crawler.TwitterAPIRequest(url, params))
 
