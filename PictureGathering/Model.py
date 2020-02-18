93573f5eb817763fde51d13542b06128ec940111
s_shift <fantasy_world4423-git@yahoo.co.jp>
2020-02-18 11:53:25 +0900

    モデル実装

--- PictureGathering/Model.py
+++ PictureGathering/Model.py
@@ -17,6 +17,22 @@ Base = declarative_base()
 
 class Favorite(Base):
     """ふぁぼツイートモデル
+
+        [id] INTEGER,
+        [is_exist_saved_file] BOOLEAN DEFAULT 'True',
+        [img_filename] TEXT NOT NULL UNIQUE,
+        [url] TEXT NOT NULL UNIQUE,
+        [url_thumbnail] TEXT NOT NULL UNIQUE,
+        [tweet_id] TEXT NOT NULL,
+        [tweet_url] TEXT NOT NULL,
+        [created_at] TEXT,
+        [user_id] TEXT NOT NULL,
+        [user_name] TEXT NOT NULL,
+        [screan_name] TEXT NOT NULL,
+        [tweet_text] TEXT,
+        [saved_localpath] TEXT,
+        [saved_created_at] TEXT,
+        PRIMARY KEY([id])
     """
     def __init__(self, *args, **kwargs):
         super(Base, self).__init__(*args, **kwargs)
@@ -39,26 +55,86 @@ class Favorite(Base):
     saved_localpath = Column(String(256))
     saved_created_at = Column(String(32))
 
-    '''
-    [id] INTEGER,
-    [is_exist_saved_file] BOOLEAN DEFAULT 'True',
-    [img_filename] TEXT NOT NULL UNIQUE,
-    [url] TEXT NOT NULL UNIQUE,
-    [url_thumbnail] TEXT NOT NULL UNIQUE,
-    [tweet_id] TEXT NOT NULL,
-    [tweet_url] TEXT NOT NULL,
-    [created_at] TEXT,
-    [user_id] TEXT NOT NULL,
-    [user_name] TEXT NOT NULL,
-    [screan_name] TEXT NOT NULL,
-    [tweet_text] TEXT,
-    [saved_localpath] TEXT,
-    [saved_created_at] TEXT,
-    PRIMARY KEY([id])
-    '''
-
     def __repr__(self):
         return "<Favorite(id='%s', img_filename='%s', url='%s')>" % (self.id, self.img_filename, self.url)
+
+
+class Retweet(Base):
+    """リツイートツイートモデル
+
+        [id] INTEGER,
+        [is_exist_saved_file] BOOLEAN DEFAULT 'True',
+        [img_filename] TEXT NOT NULL UNIQUE,
+        [url] TEXT NOT NULL UNIQUE,
+        [url_thumbnail] TEXT NOT NULL UNIQUE,
+        [tweet_id] TEXT NOT NULL,
+        [tweet_url] TEXT NOT NULL,
+        [created_at] TEXT,
+        [user_id] TEXT NOT NULL,
+        [user_name] TEXT NOT NULL,
+        [screan_name] TEXT NOT NULL,
+        [tweet_text] TEXT,
+        [saved_localpath] TEXT,
+        [saved_created_at] TEXT,
+        PRIMARY KEY([id])
+    """
+    def __init__(self, *args, **kwargs):
+        super(Base, self).__init__(*args, **kwargs)
+        self.is_exist_saved_file = True
+
+    __tablename__ = "Retweet"
+
+    id = Column(Integer, primary_key=True)
+    is_exist_saved_file = Column(Boolean, server_default=text("True"))
+    img_filename = Column(String(256), nullable=False, unique=True)
+    url = Column(String(512), nullable=False, unique=True)
+    url_thumbnail = Column(String(512), nullable=False, unique=True)
+    tweet_id = Column(String(256), nullable=False)
+    tweet_url = Column(String(512), nullable=False)
+    created_at = Column(String(32))
+    user_id = Column(String(256), nullable=False)
+    user_name = Column(String(256), nullable=False)
+    screan_name = Column(String(256), nullable=False)
+    tweet_text = Column(String(512))
+    saved_localpath = Column(String(256))
+    saved_created_at = Column(String(32))
+
+    def __repr__(self):
+        return "<Retweet(id='%s', img_filename='%s', url='%s')>" % (self.id, self.img_filename, self.url)
+
+
+class DeleteTarget(Base):
+    """削除対象ツイート保持テーブルモデル
+
+        CREATE TABLE [DeleteTarget] (
+        [id] INTEGER,
+        [tweet_id] TEXT NOT NULL UNIQUE,
+        [delete_done] BOOLEAN DEFAULT '0',
+        [created_at] DATETIME NOT NULL,
+        [deleted_at] DATETIME,
+        [tweet_text] TEXT NOT NULL,
+        [add_num] INTEGER NOT NULL,
+        [del_num] INTEGER NOT NULL,
+        PRIMARY KEY(id)
+        );
+    """
+    def __init__(self, *args, **kwargs):
+        super(Base, self).__init__(*args, **kwargs)
+        self.delete_done = False
+
+    __tablename__ = "DeleteTarget"
+
+    id = Column(Integer, primary_key=True)
+    tweet_id = Column(String(256), nullable=False, unique=True)
+    delete_done = Column(Boolean, server_default=text("False"))
+    created_at = Column(String(32), nullable=False)
+    deleted_at = Column(String(32))
+    tweet_text = Column(String(512), nullable=False)
+    add_num = Column(Integer, nullable=False)
+    del_num = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return "<DeleteTarget(id='%s', tweet_id='%s', delete_done='%s')>" % (self.id, self.tweet_id, self.delete_done)
 
 
 if __name__ == "__main__":
@@ -66,7 +142,7 @@ if __name__ == "__main__":
 
     session = Session(engine)
 
-    result = session.query(Favorite).all()[:10]
+    result = session.query(DeleteTarget).all()[:10]
     for f in result:
         print(f)
 
