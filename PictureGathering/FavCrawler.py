a5e0685b80875e6561677ca35b715fd3c77c4a7c
shift <fantasy_world4423-git@yahoo.co.jp>
2020-03-01 19:50:55 +0900

    結合テスト

--- PictureGathering/FavCrawler.py
+++ PictureGathering/FavCrawler.py
@@ -52,7 +52,7 @@ class FavCrawler(Crawler):
 
     def GetVideoURL(self, filename):
         # 'https://video.twimg.com/ext_tw_video/1139678486296031232/pu/vid/640x720/b0ZDq8zG_HppFWb6.mp4?tag=10'
-        responce = self.db_cont.DBFavVideoURLSelect("'" + filename + "'")
+        responce = self.db_cont.DBFavVideoURLSelect(filename)
         url = responce[0]["url"] if len(responce) == 1 else ""
         return url
 
