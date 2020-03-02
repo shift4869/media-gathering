e5d694a1403e06de7a654fd7f3ddd350e2e9b90c
s_shift <fantasy_world4423-git@yahoo.co.jp>
2020-03-02 16:04:24 +0900

    ログ出力の書式を変更

--- PictureGathering/RetweetCrawler.py
+++ PictureGathering/RetweetCrawler.py
@@ -125,6 +125,7 @@ class RetweetCrawler(Crawler):
         return done_msg
 
     def Crawl(self):
+        logger.info("Retweet Crawler run.")
         tweets = self.RetweetsGet()
         self.ImageSaver(tweets)
         self.ShrinkFolder(int(self.config["holding"]["holding_file_num"]))
@@ -132,7 +133,6 @@ class RetweetCrawler(Crawler):
 
 
 if __name__ == "__main__":
-    logger.info("Retweet Crawler run.")
     c = RetweetCrawler()
 
     # クロール前に保存場所から指定枚数削除しておく
