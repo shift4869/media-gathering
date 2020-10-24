c16f9f06e78b10964cb9a7733b06b883ca87b925
shift <fantasy_world4423-git@yahoo.co.jp>
2020-10-24 18:39:05 +0900

    HTML書き出し処理のテストを記述

--- test/Test_WriteHTML.py
+++ test/Test_WriteHTML.py
@@ -1,11 +1,15 @@
 # coding: utf-8
+from contextlib import ExitStack
+from mock import MagicMock, PropertyMock, patch
+import os
+import re
 import sys
 import unittest
 from logging import WARNING, getLogger
 
-from mock import MagicMock, PropertyMock, patch
+from mock import MagicMock, PropertyMock, mock_open, patch
 
-from PictureGathering import WriteHTML
+from PictureGathering import DBController, WriteHTML
 
 logger = getLogger("root")
 logger.setLevel(WARNING)
@@ -38,9 +42,9 @@ class TestWriteHTML(unittest.TestCase):
             </div>
             </th>
         '''
-        self.POINTER_PATH = './pointer.png'
-        self.FAV_HTML_PATH = './html/FavPictureGathering.html'
-        self.RETWEET_HTML_PATH = './html/RetweetPictureGathering.html'
+        self.POINTER_PATH = "./pointer.png"
+        self.FAV_HTML_PATH = "./test/FavPictureGathering.html"
+        self.RETWEET_HTML_PATH = "./test/RetweetPictureGathering.html"
         self.COLUMN_NUM = 6
 
     def tearDown(self):
@@ -52,12 +56,88 @@ class TestWriteHTML(unittest.TestCase):
     def test_MakeTHTag(self):
         """thタグ作成チェック
         """
-        pass
+        s_url = "http://sample.com"
+        s_url_thumbnail = "http://sample.com/sample_thumbnail.png"
+        s_tweet_url = "http://sample.com/tweet_url"
+
+        pic_width = 256
+        expect = self.th_template.format(pic_width=pic_width,
+                                         url=s_url,
+                                         url_thumbnail=s_url_thumbnail,
+                                         tweet_url=s_tweet_url,
+                                         pointer_path=self.POINTER_PATH)
+        expect = re.sub("\n *", "\n", expect)  # インデントによるスペースの差異は吸収する
+
+        actual = WriteHTML.MakeTHTag(s_url, s_url_thumbnail, s_tweet_url)
+        actual = re.sub("\n *", "\n", actual)  # インデントによるスペースの差異は吸収する
+
+        self.assertEqual(expect, actual)
 
     def test_WriteResultHTML(self):
         """html書き出しチェック
         """
-        pass
+        with ExitStack() as stack:
+            # open()をモックに置き換える
+            mockfout = mock_open()
+            mockfp = stack.enter_context(patch("PictureGathering.WriteHTML.open", mockfout))
+            # ファイルパスをテスト用に置き換える
+            WriteHTML.FAV_HTML_PATH = self.FAV_HTML_PATH
+            WriteHTML.RETWEET_HTML_PATH = self.RETWEET_HTML_PATH
+
+            # DBコントローラー（SELECTだけなので本番のDBを使う）
+            db_cont = DBController.DBController("J:/twitter/PG_DB.db", False)
+
+            # テスト用html生成関数
+            def MakeResultHTML(db):
+                res = ""
+                cnt = 0
+                for row in db:
+                    if cnt == 0:
+                        res += "<tr>\n"
+                    res += WriteHTML.MakeTHTag(url=row["url"], url_thumbnail=row["url_thumbnail"], tweet_url=row["tweet_url"])
+                    if cnt == self.COLUMN_NUM - 1:
+                        res += "</tr>\n"
+                    cnt = (cnt + 1) % self.COLUMN_NUM
+                if cnt != 0:
+                    for k in range((self.COLUMN_NUM) - (cnt)):
+                        res += "<th></th>\n"
+                    res += "</tr>\n"
+                return res
+
+            # Fav
+            s_db = db_cont.DBFavSelect()
+            s_save_path = self.FAV_HTML_PATH
+            res = MakeResultHTML(s_db)
+            expect = self.template.format(table_content=res)
+            expect = re.sub("\n *", "\n", expect)
+
+            res = WriteHTML.WriteResultHTML("Fav", db_cont)
+            # print(mockfout.mock_calls)
+            actual = mockfout().write.call_args[0][0]
+            actual = re.sub("\n *", "\n", actual)
+            # print(actual)
+            self.assertEqual(0, res)
+            self.assertEqual(expect, actual)
+
+            # Retweet
+            s_db = db_cont.DBRetweetSelect()
+            s_save_path = self.RETWEET_HTML_PATH
+            res = MakeResultHTML(s_db)
+            expect = self.template.format(table_content=res)
+            expect = re.sub("\n *", "\n", expect)
+
+            res = WriteHTML.WriteResultHTML("RT", db_cont)
+            # print(mockfout.mock_calls)
+            actual = mockfout().write.call_args[0][0]
+            actual = re.sub("\n *", "\n", actual)
+            # print(actual)
+            self.assertEqual(0, res)
+            self.assertEqual(expect, actual)
+
+            # エラー処理チェック
+            res = WriteHTML.WriteResultHTML("error", db_cont)
+            self.assertEqual(-1, res)
+
 
 if __name__ == "__main__":
     if sys.argv:
