b413974cc792fef3d5dcd449e7e43a37d019d6f2
shift <fantasy_world4423-git@yahoo.co.jp>
2021-02-04 14:57:40 +0900

    GoogleDriveテストをモック化

--- test/Test_GoogleDrive.py
+++ test/Test_GoogleDrive.py
@@ -40,16 +40,18 @@ class TestGoogleDrive(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # テスト用フォルダに現在のアーカイブ先フォルダ内のファイルをコピー
-        service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
-        dest_folderid = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
+        # service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
+        # dest_folderid = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
-        dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
+        # service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
+        # dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
         # テスト用フォルダ削除
         # フォルダをdeleteするとフォルダ内のファイルも警告なしで全て削除される
-        service.files().delete(fileId=dest_folderid).execute()
+        # service.files().delete(fileId=dest_folderid).execute()
+        pass
 
     def setUp(self):
         # requestsのResourceWarning抑制
@@ -74,15 +76,17 @@ class TestGoogleDrive(unittest.TestCase):
 
         # 仮想のrootフォルダ内に仮想のサンプルファイルを追加
         mimeType = "application/x-zip-compressed"
-        for i in range(0, 10):
+        for i in range(0, 11):
             res.append(self.__MakeFilesObject("sample_{}.zip".format(i), mimeType, [parents]))
         return res
 
     def __MakeFilesObject(self, name, mimeType, parents):
         td_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-        ct = datetime.now().strftime(td_format)
+        ct = (datetime.now() + timedelta(hours=-9)).strftime(td_format)
+        parents_str = "_".join(parents)
+        id_str = hashlib.md5(name.encode()).hexdigest() + hashlib.md5(parents_str.encode()).hexdigest()
         res = {
-            "id": hashlib.md5(name.encode()).hexdigest(),
+            "id": id_str,
             "name": name,
             "mimeType": mimeType,
             "trashed": False,
@@ -93,15 +97,33 @@ class TestGoogleDrive(unittest.TestCase):
 
     def __MakeCredentialsMock(self, mock):
         # "google.oauth2.service_account.Credentials.from_service_account_file" のモックを設定するヘルパ
-        # 呼び出し例：creds = Credentials.from_service_account_file(str(cred_path))
+        # 呼び出し例:creds = Credentials.from_service_account_file(str(cred_path))
         def creds_se(filename):
             return filename
         mock.side_effect = creds_se
         return mock
 
+    def __MakeMediaUploadMock(self, mock):
+        # "apiclient.http.MediaFileUpload" のモックを設定するヘルパ
+        # 呼び出し例:media = MediaFileUpload(file_path, mimetype="application/x-zip-compressed", resumable=True)
+        def mediaupload_se(file_path, mimetype, resumable):
+            res = {
+                "file_path": file_path,
+                "mimetype": mimetype,
+                "resumable": resumable
+            }
+            return res
+        mock.side_effect = mediaupload_se
+        return mock
+
     def __MakeBuildMock(self, mock):
         # "PictureGathering.GoogleDrive.build" のモックを設定するヘルパ
-        # 呼び出し例：service = build("drive", "v3", credentials=creds, cache_discovery=False)
+        # 呼び出し例:
+        # service = build("drive", "v3", credentials=creds, cache_discovery=False)
+        # results = service.files().list(q="mimeType='application/vnd.google-apps.folder'").execute()
+        # results = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+        # results = service.files().copy(body=file_metadata, fileId=item["id"]).execute()
+        # results = service.files().delete(fileId=item["id"]).execute()
         def ReturnBuild(serviceName, version, credentials, cache_discovery):
             def ReturnFiles():
                 buf_q = ""
@@ -112,13 +134,30 @@ class TestGoogleDrive(unittest.TestCase):
                     def ReturnListExecute():
                         res = {}
                         elements = []
-                        if buf_q == "mimeType='application/vnd.google-apps.folder'":
-                            mimeType = "application/vnd.google-apps.folder"
+
+                        rs = re.search("^mimeType='()'$", buf_q)
+                        if rs and rs.groups():
+                            mimeType = rs.groups()[0]
                             elements = [r for r in self.drive if r["mimeType"] == mimeType]
-                        elif buf_q == "mimeType!='application/vnd.google-apps.folder'":
-                            mimeType = "application/vnd.google-apps.folder"
+                        rs = re.search("^mimeType!='()'$", buf_q)
+                        if rs and rs.groups():
+                            mimeType = rs.groups()[0]
                             elements = [r for r in self.drive if r["mimeType"] != mimeType]
-  
+                        rs = re.search("^'(.*)' in parents$", buf_q)
+                        if rs and rs.groups():
+                            parent = rs.groups()[0]
+                            elements = [r for r in self.drive if parent in r["parents"]]
+                        rs = re.search("^mimeType='(.*)' and '(.*)' in parents$", buf_q)
+                        if rs and rs.groups():
+                            mimeType = rs.groups()[0]
+                            parent = rs.groups()[1]
+                            elements = [r for r in self.drive if r["mimeType"] == mimeType and parent in r["parents"]]
+                        rs = re.search("^name='(.*)' and mimeType='(.*)' and '(.*)' in parents$", buf_q)
+                        if rs and rs.groups():
+                            name = rs.groups()[0]
+                            mimeType = rs.groups()[1]
+                            parent = rs.groups()[2]
+                            elements = [r for r in self.drive if r["name"] == name and r["mimeType"] == mimeType and parent in r["parents"]]
                         res["files"] = elements
                         return res
 
@@ -132,6 +171,72 @@ class TestGoogleDrive(unittest.TestCase):
                 p_list = PropertyMock()
                 p_list.return_value = ReturnList
                 type(r2).list = p_list
+
+                def ReturnCreate(body, fields="", media_body=""):
+                    buf_body = body
+                    buf_media_body = media_body
+
+                    def ReturnCreateExecute():
+                        name = buf_body.get("name")
+                        mimeType = buf_body.get("mimeType")
+                        parents = buf_body.get("parents")
+
+                        r = self.__MakeFilesObject(name, mimeType, parents)
+                        self.drive.append(r)
+                        return {"id": r["id"]}
+
+                    r3 = MagicMock()
+                    p_execute = PropertyMock()
+                    p_execute.return_value = ReturnCreateExecute
+                    type(r3).execute = p_execute
+                    return r3
+
+                p_create = PropertyMock()
+                p_create.return_value = ReturnCreate
+                type(r2).create = p_create
+                
+                def ReturnCopy(body, fileId):
+                    buf_body = body
+                    buf_fileId = fileId
+
+                    def ReturnCopyExecute():
+                        record = [r for r in self.drive if r["id"] == buf_fileId][0]
+                        parents = buf_body.get("parents")
+
+                        r = self.__MakeFilesObject(record["name"], record["mimeType"], parents)
+                        self.drive.append(r)
+                        return r["id"]
+
+                    r3 = MagicMock()
+                    p_execute = PropertyMock()
+                    p_execute.return_value = ReturnCopyExecute
+                    type(r3).execute = p_execute
+                    return r3
+
+                p_copy = PropertyMock()
+                p_copy.return_value = ReturnCopy
+                type(r2).copy = p_copy
+                
+                def ReturnDelete(fileId):
+                    buf_fileId = fileId
+
+                    def ReturnDeleteExecute():
+                        records = [r for r in self.drive if r["id"] == buf_fileId]
+                        if len(records) != 1:
+                            return ""
+                        r = records[0]
+                        self.drive.remove(r)
+                        return r["id"]
+
+                    r3 = MagicMock()
+                    p_execute = PropertyMock()
+                    p_execute.return_value = ReturnDeleteExecute
+                    type(r3).execute = p_execute
+                    return r3
+
+                p_delete = PropertyMock()
+                p_delete.return_value = ReturnDelete
+                type(r2).delete = p_delete
                 return r2
 
             r1 = MagicMock()
@@ -158,8 +263,6 @@ class TestGoogleDrive(unittest.TestCase):
             actual_service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
 
             self.assertIsNotNone(actual_service)
-            # expect_service = ("drive", "v3", str(Path(CREDENTIALS_PATH)), False)
-            # self.assertEqual(expect_service, actual_service)
 
     def test_GetFolderId(self):
         """GoogleDrive内のフォルダID取得機能をチェックする
@@ -196,9 +299,9 @@ class TestGoogleDrive(unittest.TestCase):
         
             service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
             src_folderid = GoogleDrive.GetFolderId(service, SOURCE_FOLDER_NAME)
-            dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
 
-            # res = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
+            res = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
+            dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
 
             # 全ファイルを取得
             results = service.files().list(q="mimeType!='application/vnd.google-apps.folder'",
@@ -217,71 +320,88 @@ class TestGoogleDrive(unittest.TestCase):
     def test_UploadToGoogleDrive(self):
         """GoogleDriveへのアップロード機能をチェックする
         """
-        service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
-        dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
+        with ExitStack() as stack:
+            mockgdcreds = stack.enter_context(patch("google.oauth2.service_account.Credentials.from_service_account_file"))
+            mockgdbuild = stack.enter_context(patch("PictureGathering.GoogleDrive.build"))
+            mockgdmediaupload = stack.enter_context(patch("PictureGathering.GoogleDrive.MediaFileUpload"))
+            
+            mockgdcreds = self.__MakeCredentialsMock(mockgdcreds)
+            mockgdbuild = self.__MakeBuildMock(mockgdbuild)
+            mockgdmediaupload = self.__MakeMediaUploadMock(mockgdmediaupload)
 
-        # アップロードテスト用zipを作成する
-        ARCHIVE_TARGET = "test/operate_file_example"
-        src_sd = Path(ARCHIVE_TARGET)
-        dest_path = src_sd.parent / "operate_file_example.zip"
-        target_list = [sp for sp in src_sd.glob("**/*") if re.search("^(?!.*zip).*$", str(sp))]
-        if target_list:
-            with zipfile.ZipFile(dest_path, "w", compression=zipfile.ZIP_DEFLATED) as zfout:
-                for f in target_list:
-                    zfout.write(f, f.name)
+            service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
 
-        # テスト用フォルダにアップロード
-        uploaded_fileid1 = GoogleDrive.UploadToGoogleDrive(str(dest_path), CREDENTIALS_PATH, TEST_FOLDER_NAME)
-        
-        # アップロードが完了したか確認
-        results1 = service.files().list(q="name='{}' and mimeType='application/x-zip-compressed' and '{}' in parents".format(dest_path.name, dest_folderid),
-                                        fields="files(id,name,mimeType,trashed,parents,createdTime)").execute()
+            # 仮想のテスト用フォルダにコピー
+            dest_folderid = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
 
-        # テスト用フォルダにアップロード（2回目）
-        uploaded_fileid2 = GoogleDrive.UploadToGoogleDrive(str(dest_path), CREDENTIALS_PATH, TEST_FOLDER_NAME)
-        
-        # アップロードが完了したか確認（2回目）
-        results2 = service.files().list(q="name='{}' and mimeType='application/x-zip-compressed' and '{}' in parents".format(dest_path.name, dest_folderid),
-                                        fields="files(id,name,mimeType,trashed,parents,createdTime)").execute()
+            # アップロードテスト用zipを作成する
+            ARCHIVE_TARGET = "test/operate_file_example"
+            src_sd = Path(ARCHIVE_TARGET)
+            dest_path = src_sd.parent / "operate_file_example.zip"
+            target_list = [sp for sp in src_sd.glob("**/*") if re.search("^(?!.*zip).*$", str(sp))]
+            if target_list:
+                with zipfile.ZipFile(dest_path, "w", compression=zipfile.ZIP_DEFLATED) as zfout:
+                    for f in target_list:
+                        zfout.write(f, f.name)
 
-        # アップロードしたzipをGoogleDriveから削除（後始末を先にやる）
-        # 2回目のアップロード時に1回目のファイルは削除されて作り直されているはずなので2回目のファイルのみ削除
-        service.files().delete(fileId=uploaded_fileid2["id"]).execute()
+            # テスト用フォルダにアップロード
+            uploaded_fileid1 = GoogleDrive.UploadToGoogleDrive(str(dest_path), CREDENTIALS_PATH, TEST_FOLDER_NAME)
 
-        # アップロードテスト用zipを削除（後始末を先にやる）
-        dest_path.unlink()
+            # アップロードが完了したか確認
+            results1 = service.files().list(q="name='{}' and mimeType='application/x-zip-compressed' and '{}' in parents".format(dest_path.name, dest_folderid),
+                                            fields="files(id,name,mimeType,trashed,parents,createdTime)").execute()
 
-        # アップロード後の結果を確認する
-        items1 = results1.get("files")
-        self.assertEqual(1, len(items1))
-        for item in items1:
-            self.assertEqual([dest_folderid], item["parents"])
-            self.assertEqual(dest_path.name, item["name"])
+            # テスト用フォルダにアップロード（2回目）
+            uploaded_fileid2 = GoogleDrive.UploadToGoogleDrive(str(dest_path), CREDENTIALS_PATH, TEST_FOLDER_NAME)
+            
+            # アップロードが完了したか確認（2回目）
+            results2 = service.files().list(q="name='{}' and mimeType='application/x-zip-compressed' and '{}' in parents".format(dest_path.name, dest_folderid),
+                                            fields="files(id,name,mimeType,trashed,parents,createdTime)").execute()
 
-        items2 = results2.get("files")
-        self.assertEqual(1, len(items2))
-        for item in items2:
-            self.assertEqual([dest_folderid], item["parents"])
-            self.assertEqual(dest_path.name, item["name"])
+            # アップロードしたzipをGoogleDriveから削除（後始末を先にやる）
+            # 2回目のアップロード時に1回目のファイルは削除されて作り直されているはずなので2回目のファイルのみ削除
+            service.files().delete(fileId=uploaded_fileid2["id"]).execute()
 
-        self.assertEqual(items1[0]["name"], items2[0]["name"])
+            # アップロードテスト用zipを削除（後始末を先にやる）
+            dest_path.unlink()
+
+            # アップロード後の結果を確認する
+            items1 = results1.get("files")
+            self.assertEqual(1, len(items1))
+            for item in items1:
+                self.assertEqual([dest_folderid], item["parents"])
+                self.assertEqual(dest_path.name, item["name"])
+
+            items2 = results2.get("files")
+            self.assertEqual(1, len(items2))
+            for item in items2:
+                self.assertEqual([dest_folderid], item["parents"])
+                self.assertEqual(dest_path.name, item["name"])
+
+            self.assertEqual(items1[0]["name"], items2[0]["name"])
 
     def test_GoogleDriveAllDelete(self):
         """GoogleDriveのフォルダ内全削除機能をチェックする
         """
-        service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
-        dest_folderid = GoogleDrive.GetFolderId(service, TEST_FOLDER_NAME)
+        with ExitStack() as stack:
+            mockgdcreds = stack.enter_context(patch("google.oauth2.service_account.Credentials.from_service_account_file"))
+            mockgdbuild = stack.enter_context(patch("PictureGathering.GoogleDrive.build"))
+            
+            mockgdcreds = self.__MakeCredentialsMock(mockgdcreds)
+            mockgdbuild = self.__MakeBuildMock(mockgdbuild)
 
-        GoogleDrive.GoogleDriveAllDelete(TEST_FOLDER_NAME)
+            service = GoogleDrive.GetAPIService(CREDENTIALS_PATH)
 
-        # 削除確認
-        results = service.files().list(q="'{}' in parents".format(dest_folderid)).execute()
-        items = results.get("files")
+            # 仮想のテスト用フォルダにコピー
+            dest_folderid = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
 
-        self.assertEqual([], items)
+            GoogleDrive.GoogleDriveAllDelete(TEST_FOLDER_NAME)
 
-        # テスト開始時の状態に戻す
-        dest_folderid = GoogleDrive.GoogleDriveDirectoryCopy(SOURCE_FOLDER_NAME, TEST_FOLDER_NAME)
+            # 削除確認
+            results = service.files().list(q="'{}' in parents".format(dest_folderid)).execute()
+            items = results.get("files")
+
+            self.assertEqual([], items)
 
 
 if __name__ == "__main__":
