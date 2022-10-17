9c8b53c2a4666fa372d6901673d81339c3990cf3
shift <fantasy_world4423-git@yahoo.co.jp>
2022-10-17 11:04:42 +0900

    テスト追加WIP3

--- test/v2/Test_LikeFetcher.py
+++ test/v2/Test_LikeFetcher.py
@@ -1,18 +1,17 @@
 # coding: utf-8
 import codecs
 import json
+import re
 import sys
 import unittest
-import urllib
-import warnings
-from contextlib import ExitStack
-from logging import WARNING, getLogger
+from datetime import datetime, timedelta
 from mock import patch
-from pathlib import Path
 
-import requests
-
-from PictureGathering.v2.LikeFetcher import LikeFetcher
+from PictureGathering.LinkSearch.FetcherBase import FetcherBase
+from PictureGathering.LinkSearch.LinkSearcher import LinkSearcher
+from PictureGathering.LinkSearch.URL import URL
+from PictureGathering.Model import ExternalLink
+from PictureGathering.v2.LikeFetcher import LikeFetcher, LikeInfo
 from PictureGathering.v2.TwitterAPI import TwitterAPI, TwitterAPIEndpoint
 
 
@@ -62,13 +61,188 @@ class TestLikeFetcher(unittest.TestCase):
         self.assertEqual(twitter, fetcher.twitter)
 
     def test_flatten(self):
-        pass
+        userid = "12345"
+        pages = 3
+        max_results = 100
+        twitter = self._mock_twitter()
+
+        fetcher = LikeFetcher(userid, pages, max_results, twitter)
+        input_dict = self._tweet_sample()
+
+        def flatten(liked_tweet):
+            data_list = []
+            media_list = []
+            users_list = []
+            for t in liked_tweet:
+                match t:
+                    case {"data": data, "includes": {"media": media, "users": users}}:
+                        data_list.extend(data)
+                        media_list.extend(media)
+                        users_list.extend(users)
+                    case _:
+                        continue
+            return data_list, media_list, users_list
+
+        expect = flatten(input_dict)
+        actual = fetcher._flatten(input_dict)
+        self.assertEqual(expect, actual)
 
     def test_to_convert_TweetInfo(self):
-        pass
+        userid = "12345"
+        pages = 3
+        max_results = 100
+        twitter = self._mock_twitter()
+
+        fetcher = LikeFetcher(userid, pages, max_results, twitter)
+        input_dict = self._tweet_sample()
+
+        def to_convert_TweetInfo(fetcher, liked_tweet):
+            data_list, media_list, users_list = fetcher._flatten(liked_tweet)
+            result = []
+            for data in data_list:
+                match data:
+                    case {
+                        "attachments": {"media_keys": media_keys},
+                        "author_id": author_id,
+                        "created_at": created_at,
+                        "entities": {"urls": urls},
+                        "id": id_str,
+                        "source": via,
+                        "text": text
+                    }:
+                        tweet_id = id_str
+                        tweet_via = via
+                        tweet_text = text
+
+                        user_id = author_id
+                        user_name, screan_name = fetcher._find_name(user_id, users_list)
+
+                        tweet_url = fetcher._match_tweet_url(urls)
+
+                        dts_format = "%Y-%m-%d %H:%M:%S"
+                        zoned_created_at = str(created_at).replace("Z", "+00:00")
+                        utc = datetime.fromisoformat(zoned_created_at)
+                        jst = utc + timedelta(hours=9)
+                        dst = jst.strftime(dts_format)
+
+                        for media_key in media_keys:
+                            media_filename = ""
+                            media_url = ""
+                            media_thumbnail_url = ""
+
+                            media = fetcher._find_media(media_key, media_list)
+                            media_filename, media_url, media_thumbnail_url = fetcher._match_media_info(media)
+
+                            if media_filename == "" or media_url == "" or media_thumbnail_url == "":
+                                continue
+
+                            r = {
+                                "media_filename": media_filename,
+                                "media_url": media_url,
+                                "media_thumbnail_url": media_thumbnail_url,
+                                "tweet_id": tweet_id,
+                                "tweet_url": tweet_url,
+                                "created_at": dst,
+                                "user_id": user_id,
+                                "user_name": user_name,
+                                "screan_name": screan_name,
+                                "tweet_text": tweet_text,
+                                "tweet_via": tweet_via,
+                            }
+                            result.append(LikeInfo.create(r))
+                    case _:
+                        continue
+            return result
+
+        expect = to_convert_TweetInfo(fetcher, input_dict)
+        actual = fetcher.to_convert_TweetInfo(input_dict)
+        self.assertEqual(expect, actual)
+
+        expect = to_convert_TweetInfo(fetcher, [])
+        actual = fetcher.to_convert_TweetInfo([])
+        self.assertEqual(expect, actual)
 
     def test_to_convert_ExternalLink(self):
-        pass
+        class SampleLinksearcher(FetcherBase):
+            def is_target_url(self, url: URL) -> bool:
+                PIXIV_URL_PATTERN = r"^https://www.pixiv.net/artworks/[0-9]+"
+                return re.search(PIXIV_URL_PATTERN, url.original_url) is not None
+
+            def fetch(self, url: URL) -> None:
+                pass
+
+        lsb = LinkSearcher()
+        lsb.register(SampleLinksearcher())
+
+        userid = "12345"
+        pages = 3
+        max_results = 100
+        twitter = self._mock_twitter()
+
+        fetcher = LikeFetcher(userid, pages, max_results, twitter)
+        input_dict = self._tweet_sample()
+
+        def to_convert_ExternalLink(fetcher, liked_tweet, link_searcher):
+            data_list, media_list, users_list = fetcher._flatten(liked_tweet)
+            result = []
+            for data in data_list:
+                match data:
+                    case {
+                        "author_id": author_id,
+                        "created_at": created_at,
+                        "entities": {"urls": urls},
+                        "id": id_str,
+                        "source": via,
+                        "text": text
+                    }:
+                        tweet_id = id_str
+                        tweet_via = via
+                        tweet_text = text
+                        link_type = ""
+
+                        user_id = author_id
+                        user_name, screan_name = fetcher._find_name(user_id, users_list)
+
+                        tweet_url = f"https://twitter.com/{screan_name}/status/{tweet_id}"
+
+                        dts_format = "%Y-%m-%d %H:%M:%S"
+                        zoned_created_at = str(created_at).replace("Z", "+00:00")
+                        utc = datetime.fromisoformat(zoned_created_at)
+                        jst = utc + timedelta(hours=9)
+                        dst = jst.strftime(dts_format)
+
+                        saved_created_at = datetime.now().strftime(dts_format)
+
+                        expanded_urls = fetcher._match_expanded_url(urls)
+
+                        for expanded_url in expanded_urls:
+                            if not link_searcher.can_fetch(expanded_url):
+                                continue
+                            r = {
+                                "external_link_url": expanded_url,
+                                "tweet_id": tweet_id,
+                                "tweet_url": tweet_url,
+                                "created_at": dst,
+                                "user_id": user_id,
+                                "user_name": user_name,
+                                "screan_name": screan_name,
+                                "tweet_text": tweet_text,
+                                "tweet_via": tweet_via,
+                                "saved_created_at": saved_created_at,
+                                "link_type": link_type,
+                            }
+                            result.append(ExternalLink.create(r))
+                    case _:
+                        continue
+            return result
+
+        expect = to_convert_ExternalLink(fetcher, input_dict, lsb)
+        actual = fetcher.to_convert_ExternalLink(input_dict, lsb)
+        self.assertEqual(expect, actual)
+
+        expect = to_convert_ExternalLink(fetcher, [], lsb)
+        actual = fetcher.to_convert_ExternalLink([], lsb)
+        self.assertEqual(expect, actual)
 
 
 if __name__ == "__main__":
