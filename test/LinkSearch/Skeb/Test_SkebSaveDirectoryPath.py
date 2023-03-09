ab4c967991898868213911ba622b752757953a64
shift <fantasy_world4423-git@yahoo.co.jp>
2023-03-09 22:23:13 +0900

    Skebテスト追加WIP5

--- test/LinkSearch/Skeb/Test_SkebSaveDirectoryPath.py
+++ test/LinkSearch/Skeb/Test_SkebSaveDirectoryPath.py
@@ -1,27 +1,40 @@
 # coding: utf-8
-import re
-import shutil
 import sys
 import unittest
 from pathlib import Path
 
-from mock import MagicMock, mock_open, patch
-
-from PictureGathering.LinkSearch.Skeb.Authorname import Authorname
 from PictureGathering.LinkSearch.Skeb.SkebSaveDirectoryPath import SkebSaveDirectoryPath
 from PictureGathering.LinkSearch.Skeb.SkebURL import SkebURL
-from PictureGathering.LinkSearch.Skeb.SkebSaveDirectoryPath import SkebSaveDirectoryPath
 
 
 class TestSkebSaveDirectoryPath(unittest.TestCase):
     def test_SkebSaveDirectoryPath(self):
-        pass
+        base_path = Path("./test/LinkSearch/Skeb/PG_Skeb")
+        sd_path = base_path / "./author1/001/"
+        save_directory_path = SkebSaveDirectoryPath(sd_path)
+        self.assertEqual(sd_path, save_directory_path.path)
 
     def test_is_valid(self):
-        pass
+        base_path = Path("./test/LinkSearch/Skeb/PG_Skeb")
+        sd_path = base_path / "./author1/001/"
+        save_directory_path = SkebSaveDirectoryPath(sd_path)
+        self.assertTrue(save_directory_path._is_valid())
+
+        with self.assertRaises(TypeError):
+            save_directory_path = SkebSaveDirectoryPath("invalid argument")
 
     def test_create(self):
-        pass
+        skeb_url = SkebURL.create("https://skeb.jp/@author1/works/1?query=1")
+        base_path = Path("./test/LinkSearch/Skeb/PG_Skeb")
+        sd_path = base_path / "./author1/001/"
+
+        save_directory_path = SkebSaveDirectoryPath.create(skeb_url, base_path)
+        self.assertEqual(sd_path, save_directory_path.path)
+
+        with self.assertRaises(TypeError):
+            save_directory_path = SkebSaveDirectoryPath.create("invalid argument", base_path)
+        with self.assertRaises(TypeError):
+            save_directory_path = SkebSaveDirectoryPath.create(skeb_url, "invalid argument")
 
 
 if __name__ == "__main__":
